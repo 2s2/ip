20b0ce6217b7790a676c8af7e078e34c112b584d
Vyacheslav Lyovkin <v.lyovkin@astoundcommerce.com>
2016-09-26 23:46:37 +0300

    Breadcrumbs styles

--- www/wp-content/themes/CherryFramework/title.php
+++ www/wp-content/themes/CherryFramework/title.php
@@ -1,4 +1,23 @@
 <section class="title-section">
+  <?php
+    if (of_get_option('g_breadcrumbs_id') == 'yes') { ?>
+      <!-- BEGIN BREADCRUMBS-->
+      <?php
+/* Begin shop */
+        if (function_exists( 'is_shop' ) && is_shop() || function_exists( 'is_product' ) && is_product()){
+          if(class_exists( 'Woocommerce' )){
+            woocommerce_breadcrumb(array('delimiter' => ' / ', 'wrap_before' => '<ul class="breadcrumb breadcrumb__t">', 'wrap_after' => '</ul>'));
+          } elseif(function_exists( 'jigoshop_init' )){
+            jigoshop_breadcrumb('/ ', '<ul class="breadcrumb breadcrumb__t">', '</ul>');
+          }
+/* End shop */
+        } elseif (function_exists('breadcrumbs')) {
+          breadcrumbs();
+        };
+      ?>
+      <!-- END BREADCRUMBS -->
+  <?php }
+  ?>
   <h2 class="title-header">
     <?php
       $shop_page = false;
@@ -74,23 +93,4 @@
       wp_reset_query();
     } ?>
   </h2>
-  <?php
-    if (of_get_option('g_breadcrumbs_id') == 'yes') { ?>
-      <!-- BEGIN BREADCRUMBS-->
-      <?php
-/* Begin shop */
-        if (function_exists( 'is_shop' ) && is_shop() || function_exists( 'is_product' ) && is_product()){
-          if(class_exists( 'Woocommerce' )){
-            woocommerce_breadcrumb(array('delimiter' => ' / ', 'wrap_before' => '<ul class="breadcrumb breadcrumb__t">', 'wrap_after' => '</ul>'));
-          } elseif(function_exists( 'jigoshop_init' )){
-            jigoshop_breadcrumb('/ ', '<ul class="breadcrumb breadcrumb__t">', '</ul>');
-          }
-/* End shop */
-        } elseif (function_exists('breadcrumbs')) {
-          breadcrumbs();
-        };
-      ?>
-      <!-- END BREADCRUMBS -->
-  <?php }
-  ?>
 </section><!-- .title-section -->
