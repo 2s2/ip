4f29dc16f18cd11cc4531a275d67a8126caf1cf1
Vyacheslav Lyovkin <v.lyovkin@astoundcommerce.com>
2016-10-06 21:37:06 +0300

    Initial - static-banners.php

---
+++ www/wp-content/themes/CherryFramework/static/static-banners.php
@@ -0,0 +1,38 @@
+<ul class="banner-block row">
+	<li class="span4">
+		<a href="/homeplay/">
+		<img src="/wp-content/uploads/2015/04/banner1.jpg" alt="Homeplay. Играй дома" />
+		<div class="banner-block-center">
+		<div>
+		<h2>Homeplay</h2>
+		<h3>Играй дома</h3>
+		<p>Играй в любимые игры везде, где есть доступ к сети интернет!</p>
+		</div>
+		</div>
+		</a>
+	</li>
+	<li class="span4">
+		<a href="/category/casino/">
+		<img src="/wp-content/uploads/2015/04/banner2.jpg" alt="Online casino. Казино для клубов" />
+		<div class="banner-block-center">
+		<div>
+		<h2>Online casino</h2>
+		<h3>Казино для клубов</h3>
+		<p>Альтернатива игорному бизнесу - интернет клуб <br />с онлайн казино.</p>
+		</div>
+		</div>
+		</a>
+	</li>
+	<li class="span4">
+		<a href="/android/">
+		<img src="/wp-content/uploads/2015/04/banner3.jpg" alt="Android. Казино на планшетах" />
+		<div class="banner-block-center">
+		<div>
+		<h2>Android</h2>
+		<h3>Казино на планшетах</h3>
+		<p>Создайте VIP-клуб для игроков, ценящих удобство и мобильность</p>
+		</div>
+		</div>
+		</a>
+	</li>
+</ul>
