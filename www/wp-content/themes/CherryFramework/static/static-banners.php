806eacad0a133baf2f40b9a2bfbb569712dd4cba
Vyacheslav Lyovkin <v.lyovkin@astoundcommerce.com>
2016-10-06 23:19:42 +0300

    Main slider and banners

--- www/wp-content/themes/CherryFramework/static/static-banners.php
+++ www/wp-content/themes/CherryFramework/static/static-banners.php
@@ -1,7 +1,7 @@
 <ul class="banner-block row">
 	<li class="span4">
 		<a href="/homeplay/">
-		<img src="/wp-content/uploads/2015/04/banner1.jpg" alt="Homeplay. Играй дома" />
+		<img src="/wp-content/uploads/2016/10/banner1.jpg" alt="Homeplay. Играй дома" />
 		<div class="banner-block-center">
 		<div>
 		<h2>Homeplay</h2>
@@ -13,7 +13,7 @@
 	</li>
 	<li class="span4">
 		<a href="/category/casino/">
-		<img src="/wp-content/uploads/2015/04/banner2.jpg" alt="Online casino. Казино для клубов" />
+		<img src="/wp-content/uploads/2016/10/banner2.jpg" alt="Online casino. Казино для клубов" />
 		<div class="banner-block-center">
 		<div>
 		<h2>Online casino</h2>
@@ -25,7 +25,7 @@
 	</li>
 	<li class="span4">
 		<a href="/android/">
-		<img src="/wp-content/uploads/2015/04/banner3.jpg" alt="Android. Казино на планшетах" />
+		<img src="/wp-content/uploads/2016/10/banner3.jpg" alt="Android. Казино на планшетах" />
 		<div class="banner-block-center">
 		<div>
 		<h2>Android</h2>
