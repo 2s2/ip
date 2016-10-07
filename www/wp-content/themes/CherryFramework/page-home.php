d913c60807751bfec72044fa4ddc9d1fe00a3a06
Vyacheslav Lyovkin <v.lyovkin@astoundcommerce.com>
2016-10-07 18:09:40 +0300

    Initial- page-home.php

---
+++ www/wp-content/themes/CherryFramework/page-home.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Template Name: Home Page
+*/
+
+get_header(); ?>
+
+<div class="main-container">
+<div class="motopress-wrapper content-holder clearfix">
+	<div class="container">
+		<div class="row">
+			<?php do_action( 'cherry_before_home_page_content' ); ?>
+			<div class="<?php echo apply_filters( 'cherry_home_layout', 'span12' ); ?>" data-motopress-wrapper-file="page-home.php" data-motopress-wrapper-type="content">
+				<div class="row">
+					<div class="<?php echo apply_filters( 'cherry_home_layout', 'span12' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-slider.php">
+						<?php get_template_part("static/static-slider"); ?>
+					</div>
+				</div>
+				<div data-motopress-type="static" data-motopress-static-file="static/static-banners.php">
+					<?php get_template_part("static/static-banners"); ?>
+				</div>
+				<div class="row">
+					<div class="<?php echo apply_filters( 'cherry_home_layout', 'span9' ); ?>" data-motopress-type="loop" data-motopress-loop-file="loop/loop-page.php">
+						<?php get_template_part("loop/loop-page"); ?>
+					</div>
+					<div class="<?php echo cherry_get_layout_class( 'sidebar' ); ?> sidebar" id="sidebar" data-motopress-type="static-sidebar"  data-motopress-sidebar-file="sidebar.php">
+						<?php get_sidebar(); ?>
+					</div>
+				</div>
+<?php get_template_part("static/static-recent-posts"); ?>
+			</div>
+			<?php do_action( 'cherry_after_home_page_content' ); ?>
+		</div>
+	</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
